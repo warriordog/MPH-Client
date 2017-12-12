86e7fe5ecdad3eb4675b8a311a7b6fe88d66a83e
chris <acomputerdog@gmail.com>
2017-12-11 23:07:42 -0500

    Add tool to check ruby syntax

---
+++ tool/check.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Bash script to check ruby source for errors
+
+# File pattern to search for
+pattern="*.rb"
+
+# Directory to search in
+dir="../src"
+
+# Read arguments    
+if [ $# -gt 0 ]; then
+    $dir = $1
+fi
+if [ $# -gt 1 ]; then
+    $pattern = $2
+fi
+
+# Find files
+for fl in $dir/$pattern; do
+    echo -n "Checking $fl: "
+    
+    # Run ruby check
+    output=$(ruby -c -w $fl 2>&1)
+    
+    # compare output
+    if [ "$output" == "Syntax OK" ]; then
+        echo "OK"
+    else
+        echo "failed!"
+        echo $output
+        exit 1
+    fi
+done
+
+echo "Everything OK"
