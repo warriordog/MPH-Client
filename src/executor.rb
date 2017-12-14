04191b2f2024f434beabc94f5a9e2562727ce81e
chris <acomputerdog@gmail.com>
2017-12-14 02:41:57 -0500

    Use pipes instead of PTY on windows

--- src/executor.rb
+++ src/executor.rb
@@ -2,9 +2,6 @@
 # Executes a miner in a different process
 #-----------------------------------------
 
-# Have to use PTYs because *SOME* miners don't know how to talk to a pipe
-require 'pty'
-
 # Not required, because worker requires this
 #require_relative('worker')
 require_relative 'log'
@@ -33,60 +30,70 @@ class Executor
             cmd = "#{@job.miner.exec} #{@job.miner.args(job)}"
             @logger.debug("Command: #{cmd}")
 
-            # Adpated from https://ruby-doc.org/stdlib-2.2.3/libdoc/pty/rdoc/PTY.html
-            master, slave = PTY.open
-            @pid = spawn(cmd, :chdir => @job.miner.path, :in=>slave, :out=>slave, :err=>slave)
-            slave.close    # Don't need the slave
-
-            @running = true
-
-            # Thread to read from process
-            Thread.new {
-                begin
+            # Linux and windows miners behave differently and need different process code.
+            if (Gem.win_platform?)
+                @logger.debug("Using pipes for IO")
+                # This method doesn't work for programs that use TTYs
+                # Start the process
+                stdin, stdout, thr = Open3.popen2e(cmd, :chdir => @job.miner.path)
+                @pid = thr.pid # pid of the started process.
+                @running = true
+                
+                # Close write pipe
+                stdin.close()
+                    
+                # Read from new thread
+                Thread.new {
                     # Read until pipe closes
-                    until (rawLine = master.gets()).nil? do
+                    until (rawLine = stdout.gets).nil? do
                         line = rawLine.chomp() # Get rid of trailing newline
                         @logger.info(line)
                     end
-                rescue e
-                    @logger.warn("Exception in read thread: #{e}")
-                ensure
-                    @running = false
-                    master.close()
-                    @logger.info("Process ended.")
-                end
-            }
-# This method doesn't work for programs that use TTYs
-=begin
-            # Start the process
-            stdin, stdout, thr = Open3.popen2e(cmd, :chdir => @algorithm.miner.path)
-            #stdout.sync = true
-            @pid = thr.pid # pid of the started process.
-            @running = true
-                
-            # Read from new thread
-            Thread.new {
-                # Read until pipe closes
-                until (rawLine = stdout.gets).nil? do
-                    line = rawLine.chomp() # Get rid of trailing newline
-                    @logger.info(line)
-                end
-                @logger.debug("Read thread ended")
-            }
+                    @logger.debug("Read thread ended")
+                }
 
-            Thread.new {
-                # Join is necessary or else the pipes will just swallow all data.
-                # Not even a while loop will work, wtf!
-                # But, we can put the join in its own thread and not block up the main program
-                thr.join
+                Thread.new {
+                    # Join is necessary or else the pipes will just swallow all data.
+                    # But, we can put the join in its own thread and not block up the main program
+                    thr.join
+                    
+                    # join returns when process finishes
+                    @running = false
+                    @logger.info("Process terminated.")
+                }
                 
-                # join returns when process finishes
-                @running = false
-                @logger.info("Process terminated.")
-            }
-            
-            @logger.debug("Finished starting")
-=end
+                @logger.debug("Finished starting")
+            # For anything other than windows, use a PTY
+            else
+                @logger.debug("Using PTY for IO")
+                # Have to use PTYs because *SOME* miners don't know how to talk to a pipe
+                require 'pty'
+
+                # Adpated from https://ruby-doc.org/stdlib-2.2.3/libdoc/pty/rdoc/PTY.html
+                master, slave = PTY.open
+                @pid = spawn(cmd, :chdir => @job.miner.path, :in=>slave, :out=>slave, :err=>slave)
+                slave.close    # Don't need the slave
+                
+                @running = true
+
+                # Thread to read from process
+                Thread.new {
+                    begin
+                        # Read until pipe closes
+                        until (rawLine = master.gets()).nil? do
+                            line = rawLine.chomp() # Get rid of trailing newline
+                            @logger.info(line)
+                        end
+                    rescue e
+                        @logger.warn("Exception in read thread: #{e}")
+                    ensure
+                        @running = false
+                        master.close()
+                        @logger.info("Process ended.")
+                    end
+                }
+
+            end
         end
     end
     
