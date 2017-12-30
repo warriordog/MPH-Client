9df5c5623ed15eaa775ce9414336008388b3b951
chris <acomputerdog@gmail.com>
2017-12-30 00:28:07 -0500

    Better error handling in MPH API

--- src/api/mph.rb
+++ src/api/mph.rb
@@ -18,29 +18,38 @@ module MPH
             url = "https://#{coin != nil ? coin + "." : ""}miningpoolhub.com/index.php?page=api&action=getminingandprofitsstatistics"
             
             # Convert to URI
-            uri = URI(url)
+            uri = URI.parse(url)
             
             # send request
-            json = Net::HTTP.get(uri)
+            resp = Net::HTTP.get(uri)
             
-            # Parse json
-            resp = JSON.parse(json, :symbolize_names => true)
-            
-            # Check for success
-            if (resp[:success] == true)
-                # if "return" is missing, then this will just return nil, which is the error status anyway
-                return resp[:return]
+            # Check for network errors
+            if (resp != nil)
+                # Parse json
+                json = JSON.parse(resp, :symbolize_names => true)
+                
+                # Check for success
+                if (json[:success] == true)
+                    # if "return" is missing, then this will just return nil, which is the error status anyway
+                    return json[:return]
+                else
+                    # Return nil if server had error
+                    @@logger.warn("Server error in getminingandprofitsstatistics: '#{resp[:return]}'")
+                    return nil
+                end
             else
-                # Return nil if server had error
-                @@logger.warn("Server error in getminingandprofitsstatistics: '#{resp[:return]}'")
-                return nil
+                @@logger.error("Network error while getting mining statistics.")
             end
-        # TODO proper error checking
-        # Return nil in case of errors
+        rescue URI::InvalidURIError => e
+            @@logger.error("Invalid URI generated for MPH API, was a bad coin entered?  Error was #{e}")
+        rescue JSON::JSONError => e
+            @@logger.error("Invalid JSON returned from MPH: #{e}")
         rescue Exception => e
             @@logger.error("Error in getminingandprofitsstatistics: #{e}")
-            return nil
         end
+        
+        # Return nil in case of errors
+        return nil
     end
 	
 	# converts an MPH rate into H/s
