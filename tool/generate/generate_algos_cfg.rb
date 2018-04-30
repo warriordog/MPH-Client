84673f3dd36129f9f1227345df778034964651cb
warriordog <warriordog@github.com>
2018-04-29 20:31:05 -0400

    Update generate algos for new config format

--- tool/generate/generate_algos_cfg.rb
+++ tool/generate/generate_algos_cfg.rb
@@ -7,20 +7,18 @@ require 'net/http'
 json = Net::HTTP.get(URI("https://miningpoolhub.com/index.php?page=api&action=getminingandprofitsstatistics"))
 resp = JSON.parse(json)
 
-if (resp["success"])
-    stats = resp["return"]
+if (resp['success'])
+    stats = resp['return']
     
-    algos = Hash.new { |hash, key| hash[key] = {"id" => key, "name" => key, "coins" => []} }
+    algos = Hash.new { |hash, key| hash[key] = {'name' => key, 'coins' => {}} }
     
     stats.each {|statCoin| 
-        coin = {}
-        coin["id"] = statCoin["coin_name"]
-        coin["name"] = coin["id"]
+        coin = {'name' => statCoin['coin_name']}
         
-        algos[statCoin["algo"].downcase]["coins"] << coin
+        algos[statCoin['algo'].downcase]['coins'][statCoin['coin_name']] = coin
     }
     
-    puts algos.values.to_json    
+    puts JSON.pretty_generate(algos.values, {:indent => '  '})
 else
     puts "Server returned error"
 end
